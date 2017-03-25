d3542af7975a517dbd5d254c75fb85f34f9dc452
nel garrata red <arnelgarratajr@outlook.com>
2017-03-25 12:39:15 +0800

    Initial commit

---
+++ UnderstandLINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> myCars = new List<Car>()
+            {
+                new Car() { Make = "BMW", Model = "550i", Year = 2009, Color = CarColor.Black, StickerPrice = 55000 },
+                new Car() { Make = "Toyota", Model = "4Runner", Year = 2010, Color = CarColor.Blue, StickerPrice = 35000 },
+                new Car() { Make = "BWM", Model = "745li", Year = 2008, Color = CarColor.Red, StickerPrice = 75000 },
+                new Car() { Make = "Ford", Model = "Escape", Year = 2008, Color = CarColor.White, StickerPrice = 25000 },
+                new Car() { Make = "BMW", Model = "55i", Year = 2010, Color = CarColor.Yellow, StickerPrice = 57000 }
+            };
+
+            foreach (var car in myCars)
+            {
+                Console.WriteLine("Brand: {0} Model: {1}", car.Make, car.Model);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double StickerPrice { get; set; }
+        public CarColor Color { get; set; }        
+    }
+
+    enum CarColor
+    {
+        Black,
+        White,
+        Red,
+        Blue,
+        Yellow
+    }
+}
